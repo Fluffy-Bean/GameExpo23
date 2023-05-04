dbd6a81c784b7b9647b735ab064692254e34c214
Michał Gdula <michal-gdula@protonmail.com>
2023-05-04 11:57:02 +0300

    Save database data

--- Highscore-Server/highscore/config.py
+++ Highscore-Server/highscore/config.py
@@ -2,3 +2,6 @@ SECRET_KEY = 'dev'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+MIGRATION_DIR = '/data/storage/migrations'
+INSTANCE_DIR = '/data/storage/instance'
--- Highscore-Server/highscore/server.py
+++ Highscore-Server/highscore/server.py
@@ -1,18 +1,24 @@
+import os
+
 from flask import Flask
 from flask_migrate import init as migrate_init
 from extensions import db, migrate, cache
+from config import MIGRATION_DIR, INSTANCE_DIR
 from views import blueprint
 
-
-app = Flask(__name__)
+app = Flask(__name__, instance_path=INSTANCE_DIR)
 app.config.from_pyfile('config.py')
 
 db.init_app(app)
-migrate.init_app(app, db)
+migrate.init_app(app, db, directory=MIGRATION_DIR)
 cache.init_app(app)
 
-with app.app_context():
-    db.create_all()
-    migrate_init()
+if not os.path.exists(os.path.join(INSTANCE_DIR, 'db.sqlite')):
+    with app.app_context():
+        db.create_all()
+
+if not os.path.exists(MIGRATION_DIR):
+    with app.app_context():
+        migrate_init(directory=MIGRATION_DIR)
 
 app.register_blueprint(blueprint)
