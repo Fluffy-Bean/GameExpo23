8bf194f936b3dc2ad1447a1702202be4f121fd6d
Michał Gdula <michal-gdula@protonmail.com>
2023-05-04 18:05:17 +0300

    Fix runner

--- Highscore-Server/highscore/run.sh
+++ Highscore-Server/highscore/run.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 
+# Wait for database to start
 until pg_isready -d $DB_NAME -h $DB_HOST -p $DB_PORT -U $DB_USER
 do
     echo "Waiting for database to start... (5s)"
     sleep 5
 done
 
-echo "Database is ready! Creating tables..."
-flask --app server db init
+echo "Database is ready!"
 
-if [ -n $(flask --app server db check) ]
+# Check if migrastions folder exists
+if [ ! -d "migrations" ]
+then
+    echo "Creating tables..."
+    flask --app server db init
+fi
+
+# Check if there are any changes to the database
+if -n flask --app server db check
 then
     echo "Database changes detected! Migrating..."
     flask --app server db migrate
     flask --app server db upgrade
 fi
 
+# Start server!!!!
 echo "Starting server..."
 gunicorn --bind highscore:8080 server:app
